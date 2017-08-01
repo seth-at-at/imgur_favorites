be55ddb4e9db33ee3a1ea8ed90cdb77b7077f4e8
Seth <seth19483@gmail.com>
2017-08-01 08:36:55 -0600

    fix es6

---
+++ app/assets/javascripts/areUser.js
@@ -0,0 +1,9 @@
+function askUser(){
+  var username = document.location.hash.split('&')[4].split('=')[1]
+  $('#are_user')[0].append('Are you ' + username + '?')
+
+}
+
+$(document).ready(function(){
+  askUser()
+})
