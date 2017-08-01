50b913fd39348082adf501590563010a815186f0
Seth <seth19483@gmail.com>
2017-08-01 09:17:20 -0600

    pc

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,10 @@
 class FavoritesController < ApplicationController
+  require 'will_paginate/array'
+
   def index
     @favorites = FavoritesUser.where(user_id: current_user.id).map do |fu|
       Favorite.find(fu.favorite_id)
-    end
+    end.paginate(:page => params[:page], :per_page => 16)
     @folders = Folder.where(user: current_user)
   end
 end
