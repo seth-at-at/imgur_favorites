93e8942cc044dafd7a4be8f4371d7d13ba3eaf67
Seth <seth19483@gmail.com>
2017-08-01 07:45:36 -0600

    fix folders

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 module ImgurFolders
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
