25051a2e387369bf7584a0443aa1b634e3110d77
Seth <seth19483@gmail.com>
2017-08-12 15:54:58 -0600

    idk

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,13 +11,8 @@ class SessionsController < ApplicationController
 
   def create
     par = param_arr(params[:params])
-    logger.debug(par) # DEBUG
-    logger.debug("15") # DEBUG
-
 
     favorites = ImgurService.new.favorites(par["account_username"], par["access_token"])
-    logger.debug(favorites) # DEBUG
-    logger.debug("20") # DEBUG
 
     user = User.find_or_create_by(username: par["account_username"])
     logger.debug(user) # DEBUG
